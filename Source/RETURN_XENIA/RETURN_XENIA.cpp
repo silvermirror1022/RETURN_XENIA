1441f83617241a3641a941a69666de42603a4348
gdevhun <wjh9330@naver.com>
2024-10-14 16:51:30 +0900

    Init2

---
+++ Source/RETURN_XENIA/RETURN_XENIA.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RETURN_XENIA.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RETURN_XENIA, "RETURN_XENIA" );
+ 
