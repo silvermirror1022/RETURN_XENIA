a4d801a0dca7c4a0176b9a5b56414f8b44ddc03b
gdevhun <wjh9330@naver.com>
2024-10-14 16:20:48 +0900

    Init Project

---
+++ Intermediate/Build/Win64/x64/RETURN_XENIAEditor/Development/Engine/SharedPCH.Engine.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "D:/RETURN_XENIA/RETURN_XENIA/Intermediate/Build/Win64/x64/RETURN_XENIAEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
---
+++ Source/RETURN_XENIA/RETURN_XENIAGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RETURN_XENIAGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARETURN_XENIAGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARETURN_XENIAGameMode();
+};
+
+
+
