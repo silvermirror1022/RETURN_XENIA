52c139155f69f1db7b3d9b41fbdbde617ea16fb0
gdevhun <wjh9330@naver.com>
2024-10-31 14:53:58 +0900

    Feat : Harpy FireBall

---
+++ Source/RETURN_XENIA/Character/RXNonPlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/RXCharacterBase.h"
+#include "RXNonPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RETURN_XENIA_API ARXNonPlayer : public ARXCharacterBase
+{
+	GENERATED_BODY()
+	
+};
