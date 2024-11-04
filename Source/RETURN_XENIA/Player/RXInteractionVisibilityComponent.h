8802f3605322739a9eec1072023aafe310f90a61
gdevhun <wjh9330@naver.com>
2024-11-04 16:47:44 +0900

    Feat: InteractionComponent

---
+++ Source/RETURN_XENIA/Player/RXInteractionVisibilityComponent.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "RXInteractionVisibilityComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class RETURN_XENIA_API URXInteractionVisibilityComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+
+	URXInteractionVisibilityComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+		const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+public:	
+private:
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<class USphereComponent> SphereComponent;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<class UWidgetComponent> WidgetComponent;
+
+	UPROPERTY(EditAnywhere, Category = ParentWidgetBP)
+	TSubclassOf<UUserWidget> WidgetClass;
+		
+};
