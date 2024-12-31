939ba6a3c2acc193224e18e781f74beca212adee
gdevhun <wjh9330@naver.com>
2024-12-31 23:10:55 +0900

    Feat: TemplePuzzel Hierachy

--- Source/RETURN_XENIA/Actor/RXPuzzelBase.cpp
+++ Source/RETURN_XENIA/Actor/RXPuzzelBase.cpp
@@ -34,6 +34,7 @@ void ARXPuzzelBase::PuzzelEventStart_Implementation()
  * PuzzelEventFinish함수는 해당 Base가 가진 자식 컴포넌트 매시, 인터렉션비지빌리티등을 쉽게 제거하기 위해
  * 블루프린트에서 진행하기 위해서 BlueprintNativeEvent로 혼합,
  * 실제로 컴포넌트에서도 퍼즐종료함수를 부르고 이 함수를 불러줘야 끝나게 됨.
+ * 블루프린트 퍼즐이벤트메니지컴포넌트의 마지막 부분 흐름에서 호출 중임.
  */
 
 void ARXPuzzelBase::PuzzelEventFinish_Implementation()
