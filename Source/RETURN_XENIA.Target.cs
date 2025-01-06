53412b330f2dd7512e7a6336a353c28003fab6c2
gdevhun <wjh9330@naver.com>
2025-01-06 17:22:56 +0900

    version 5.44

--- Source/RETURN_XENIA.Target.cs
+++ Source/RETURN_XENIA.Target.cs
@@ -8,8 +8,8 @@ public class RETURN_XENIATarget : TargetRules
 	public RETURN_XENIATarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("RETURN_XENIA");
 	}
 }
--- Source/RETURN_XENIAEditor.Target.cs
+++ Source/RETURN_XENIAEditor.Target.cs
@@ -8,8 +8,8 @@ public class RETURN_XENIAEditorTarget : TargetRules
 	public RETURN_XENIAEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("RETURN_XENIA");
 	}
 }
