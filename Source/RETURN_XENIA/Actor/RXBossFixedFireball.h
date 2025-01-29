daccecef29036f6a677d8df507727cdf348c4edb
gdevhun <wjh9330@naver.com>
2025-01-29 23:48:13 +0900

    Feat: BossBT, ObjectPooling Introduce

---
+++ Source/RETURN_XENIA/Actor/RXBossFixedFireball.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RXBossFixedFireball.generated.h"
+
+UCLASS()
+class RETURN_XENIA_API ARXBossFixedFireball : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+
+	ARXBossFixedFireball();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+public:	
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Speed")
+	float Speed;
+
+	void LaunchProjectile(FVector Direction);
+
+	UFUNCTION(BlueprintCallable)
+	void ResetFireball();
+
+private:
+	UPROPERTY(EditAnywhere, Category = "ProjectileMovement")
+	TObjectPtr<class UProjectileMovementComponent> ProjectileMovement;
+
+	FTimerHandle DeactivationTimer;
+
+	FVector InitialLocation;
+};
