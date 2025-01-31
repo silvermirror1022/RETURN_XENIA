31f426524eaa9e23d66d63970c2c7564b89e75fe
gdevhun <wjh9330@naver.com>
2025-02-01 04:15:45 +0900

    Fix TimingPuzzel

--- Source/RETURN_XENIA/Character/RXPlayer.cpp
+++ Source/RETURN_XENIA/Character/RXPlayer.cpp
@@ -265,8 +265,8 @@ void ARXPlayer::Look(const FInputActionValue& Value)
 
 	if (Controller != nullptr)
 	{
-		AddControllerYawInput(LookAxisVector.X * 0.25f);
-		AddControllerPitchInput(LookAxisVector.Y * 0.25f);
+		AddControllerYawInput(LookAxisVector.X * 0.3f);
+		AddControllerPitchInput(LookAxisVector.Y * 0.3f);
 	}
 }
 void ARXPlayer::StartSprinting()
