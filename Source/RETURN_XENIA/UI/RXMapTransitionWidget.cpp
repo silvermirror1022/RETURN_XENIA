70debdb2a641d063089444b154c6d5f8bc9c36c3
gdevhun <wjh9330@naver.com>
2025-02-08 06:35:56 +0900

    Feat: MapTransitionUI

--- Source/RETURN_XENIA/UI/RXMapTransitionWidget.cpp
+++ Source/RETURN_XENIA/UI/RXMapTransitionWidget.cpp
@@ -1,7 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "UI/RXMapTransitionWidget.h"
+
+#include "Components/CanvasPanelSlot.h"
 #include "Components/Image.h"
 #include "Engine/Texture2D.h"
 
@@ -24,5 +26,14 @@ void URXMapTransitionWidget::SetMapTransitionImageByName(FName MapName, bool IsK
 	if (FoundImage && *FoundImage)
 	{
 		StageNameImage->SetBrushFromTexture(*FoundImage);
+
+		//  텍스처 크기를 가져오기
+		FVector2D ImageSize((*FoundImage)->GetSizeX(), (*FoundImage)->GetSizeY());
+
+		// 부모 슬롯 가져오기
+		if (UCanvasPanelSlot* CanvasSlot = Cast<UCanvasPanelSlot>(StageNameImage->Slot))
+		{
+			CanvasSlot->SetSize(ImageSize);  // 슬롯 크기 설정
+		}
 	}
 }
