54aa47671081a400f2b8d2ae6a9b15328ed4a884
gdevhun <wjh9330@naver.com>
2025-02-16 02:10:04 +0900

    Feat: InventorySystem UI, CppClass

--- Source/RETURN_XENIA/UI/RXMainMenuWidget.cpp
+++ Source/RETURN_XENIA/UI/RXMainMenuWidget.cpp
@@ -1,6 +1,7 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "UI/RXMainMenuWidget.h"
+#include "UI/Inventory/RXInventorySlotWidget.h"
 
 URXMainMenuWidget::URXMainMenuWidget(const FObjectInitializer& ObjectInitializer)
 	:Super(ObjectInitializer)
