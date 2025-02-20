a680eeccd885b83496915e3b5b4118e903fabe1a
gdevhun <wjh9330@naver.com>
2025-02-21 03:18:11 +0900

    Feat: Memory UI

--- Source/RETURN_XENIA/UI/RXMainMenuWidget.cpp
+++ Source/RETURN_XENIA/UI/RXMainMenuWidget.cpp
@@ -1,10 +1,10 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "UI/RXMainMenuWidget.h"
-#include "UI/Inventory/RXInventorySlotWidget.h"
+#include "Components/Image.h"
 
 URXMainMenuWidget::URXMainMenuWidget(const FObjectInitializer& ObjectInitializer)
-	:Super(ObjectInitializer)
+	: Super(ObjectInitializer)
 {
 }
 
@@ -13,3 +13,87 @@ void URXMainMenuWidget::NativeConstruct()
 	Super::NativeConstruct();
 }
 
+// 모든 PressedImage를 숨기는 함수
+void URXMainMenuWidget::ResetAllPressedImages() const
+{
+  
+	TArray<TObjectPtr<UImage>> AllPressedImages = {
+		Ch1_PressedImg_EN, Ch1_PressedImg_KR,
+        Ch2_PressedImg_EN, Ch2_PressedImg_KR,
+        Ch3_PressedImg_EN, Ch3_PressedImg_KR,
+        Ch4_PressedImg_EN, Ch4_PressedImg_KR,
+        Ch5_PressedImg_EN, Ch5_PressedImg_KR
+	};
+
+	for (UImage* Img : AllPressedImages)
+	{
+		if (Img)
+		{
+			Img->SetVisibility(ESlateVisibility::Hidden);
+		}
+	}
+}
+void URXMainMenuWidget::OnCh1BtnPressed(bool bIsKorean)
+{
+    ResetAllPressedImages();
+    if (bIsKorean && Ch1_PressedImg_KR)
+    {
+        Ch1_PressedImg_KR->SetVisibility(ESlateVisibility::Visible);
+    }
+    else if (!bIsKorean && Ch1_PressedImg_EN)
+    {
+        Ch1_PressedImg_EN->SetVisibility(ESlateVisibility::Visible);
+    }
+}
+
+void URXMainMenuWidget::OnCh2BtnPressed(bool bIsKorean)
+{
+    ResetAllPressedImages();
+    if (bIsKorean && Ch2_PressedImg_KR)
+    {
+        Ch2_PressedImg_KR->SetVisibility(ESlateVisibility::Visible);
+    }
+    else if (!bIsKorean && Ch2_PressedImg_EN)
+    {
+        Ch2_PressedImg_EN->SetVisibility(ESlateVisibility::Visible);
+    }
+}
+
+void URXMainMenuWidget::OnCh3BtnPressed(bool bIsKorean)
+{
+    ResetAllPressedImages();
+    if (bIsKorean && Ch3_PressedImg_KR)
+    {
+        Ch3_PressedImg_KR->SetVisibility(ESlateVisibility::Visible);
+    }
+    else if (!bIsKorean && Ch3_PressedImg_EN)
+    {
+        Ch3_PressedImg_EN->SetVisibility(ESlateVisibility::Visible);
+    }
+}
+
+void URXMainMenuWidget::OnCh4BtnPressed(bool bIsKorean)
+{
+    ResetAllPressedImages();
+    if (bIsKorean && Ch4_PressedImg_KR)
+    {
+        Ch4_PressedImg_KR->SetVisibility(ESlateVisibility::Visible);
+    }
+    else if (!bIsKorean && Ch4_PressedImg_EN)
+    {
+        Ch4_PressedImg_EN->SetVisibility(ESlateVisibility::Visible);
+    }
+}
+
+void URXMainMenuWidget::OnCh5BtnPressed(bool bIsKorean)
+{
+    ResetAllPressedImages();
+    if (bIsKorean && Ch5_PressedImg_KR)
+    {
+        Ch5_PressedImg_KR->SetVisibility(ESlateVisibility::Visible);
+    }
+    else if (!bIsKorean && Ch5_PressedImg_EN)
+    {
+        Ch5_PressedImg_EN->SetVisibility(ESlateVisibility::Visible);
+    }
+}
--- Source/RETURN_XENIA/UI/RXMainMenuWidget.h
+++ Source/RETURN_XENIA/UI/RXMainMenuWidget.h
@@ -35,7 +35,45 @@ public:
 	UPROPERTY(meta = (BindWidget), BlueprintReadOnly)
 	TObjectPtr<UButton> Btn_NoteDesc; // 노트 아이템 설명창 버튼
 
-	
+	/*
+	 * 메모리 섹션 변수 및 함수
+	 * 버튼 활성화 / 비활성화 이미지
+	 */
 
+	UFUNCTION(BlueprintCallable)
+	void OnCh1BtnPressed(bool bIsKorean);
+	UFUNCTION(BlueprintCallable)
+	void OnCh2BtnPressed(bool bIsKorean);
+	UFUNCTION(BlueprintCallable)
+	void OnCh3BtnPressed(bool bIsKorean);
+	UFUNCTION(BlueprintCallable)
+	void OnCh4BtnPressed(bool bIsKorean);
+	UFUNCTION(BlueprintCallable)
+	void OnCh5BtnPressed(bool bIsKorean);
+
+private:
+	// 모든 버튼의 PressedImage를 리셋하는 함수
+	void ResetAllPressedImages() const;
+
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UImage> Ch1_PressedImg_EN;
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UImage> Ch1_PressedImg_KR;
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UImage> Ch2_PressedImg_EN;
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UImage> Ch2_PressedImg_KR;
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UImage> Ch3_PressedImg_EN;
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UImage> Ch3_PressedImg_KR;
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UImage> Ch4_PressedImg_EN;
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UImage> Ch4_PressedImg_KR;
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UImage> Ch5_PressedImg_EN;
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UImage> Ch5_PressedImg_KR;
 	
 };
